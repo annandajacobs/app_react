ebb7a0227d1d733db2b58fb094e3cd59a241e566
annandajacobs <annandajacobs@gmail.com>
2024-10-16 17:26:25 -0300

    initial commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX = como o react lê o código e transforma em document
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo." />
+        </header>
+        
+    )
+}
+
+export default Banner;
