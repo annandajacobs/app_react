803e9de7cc18d49bf78b5fff72b82253dd42e9f9
annandajacobs <annandajacobs@gmail.com>
2024-10-25 14:38:37 -0300

    up3

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -2,9 +2,9 @@ import './Rodape.css'
 
 function Rodape() {
     return (
-        <footer className='rodape'>
-            <p>&copy; {new Date().getFullYear()} Meu Site. Todos os direitos reservados.</p>
-            <img id='logo' src="/imagens/logo.png" alt="Logo" />
+        <footer className='footer'>
+            <p>&copy; {new Date().getFullYear()} Meu Site. </p>
+            <img className='logo' src="/imagens/logo.png" alt="Logo" />
             <div className='redes'>
                 <img src="/imagens/ig.png" alt="ig" />
                 <img src="/imagens/tw.png" alt="tw" />
